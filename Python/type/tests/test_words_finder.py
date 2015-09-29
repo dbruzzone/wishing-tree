67d57c9de6cb125e678e436c8c426790464dd484
Davide Bruzzone <davide.bruzzone@gmail.com>
2015-09-29 13:50:15 -0400

    Added the ability to find words that contain any of the specified characters

--- Python/type/tests/test_words_finder.py
+++ Python/type/tests/test_words_finder.py
@@ -6,15 +6,21 @@ class TestWordFinder(unittest.TestCase):
 
     _words = [ 'AAA', 'AAB', 'ABB', 'BBB', 'BBC', 'BCC', 'CCC', 'BDD' ]
 
-    def test_find_words_that_contain(self):
+    _finder = Finder()
+
+    def test_find_words_that_contain_any(self):
         self.assertEqual(len(self._words), 8)
 
-        finder = Finder()
+        self.assertEqual(len(self._finder.words_that_contain_any(self._words, 'Z')), 0)
+        self.assertEqual(len(self._finder.words_that_contain_any(self._words, 'A')), 1)
+        self.assertEqual(len(self._finder.words_that_contain_any(self._words, 'AB')), 4)
+        self.assertEqual(len(self._finder.words_that_contain_any(self._words, 'BD')), 2)
 
-        self.assertEqual(len(finder.words_that_contain(self._words, 'Z')), 0)
-        self.assertEqual(len(finder.words_that_contain(self._words, 'A')), 1)
-        self.assertEqual(len(finder.words_that_contain(self._words, 'AB')), 2)
-        self.assertEqual(len(finder.words_that_contain(self._words, 'BD')), 1)
+    def test_find_words_that_contain_all(self):
+        self.assertEqual(len(self._finder.words_that_contain_all(self._words, 'Z')), 0)
+        self.assertEqual(len(self._finder.words_that_contain_all(self._words, 'A')), 1)
+        self.assertEqual(len(self._finder.words_that_contain_all(self._words, 'AB')), 2)
+        self.assertEqual(len(self._finder.words_that_contain_all(self._words, 'BD')), 1)
 
 if __name__ == '__main__':
     unittest.main()
--- Python/type/words/finder.py
+++ Python/type/words/finder.py
@@ -1,26 +1,29 @@
+import collections
+
 class Finder:
-    def words_that_contain(self, words, characters):
+    def words_that_contain_any(self, words, characters):
         result = []
 
         # Loop through words...
         for word in words:
-            checked_characters = "" 
-            include_word = True
+            word_contains = self._word_contains(word, characters)
+            include_word = word_contains[0]
 
-            # ...Then through each character in word
-            for character in word:
-                # If character hasn't previously been checked (If it has, skip it to speed the process
-                # up a bit)...
-                if character not in checked_characters:
-                    # ...And it isn't in the list of characters that word must contain...
-                    if character not in characters:
-                        # ...Don't include word in result
-                        include_word = False
+            if include_word:
+                result.append(word) 
 
-                    checked_characters += character
+        return result
+
+    def words_that_contain_all(self, words, characters):
+        result = []
+
+        # Loop through words...
+        for word in words:
+            word_contains = self._word_contains(word, characters)
+            include_word = word_contains[0]
 
             # Have all the characters in characters been checked?...
-            if len(characters) != len(checked_characters):
+            if len(characters) != len(word_contains[1]):
                 # If they haven't, word does not and cannot contain all the characters in characters.
                 # This catches (admitedly unlikely) cases like "Does 'BBB' contain 'BD'?" (It
                 # shouldn't)
@@ -30,3 +33,20 @@ class Finder:
                 result.append(word) 
 
         return result
+
+    def _word_contains(self, word, characters):
+        result = True
+        checked_characters = ""
+
+        for character in word:
+            # If character hasn't previously been checked (If it has, skip it to speed the process
+            # up a bit)...
+            if character not in checked_characters:
+                # ...And it isn't in the list of characters that word must contain...
+                if character not in characters:
+                    # ...Don't include word in result
+                    result = False
+
+                checked_characters += character
+
+        return (result, checked_characters)
