4c69d3f32cc8d25dd668c427aa845a160ca32538
Davide Bruzzone <davide.bruzzone@gmail.com>
2015-10-20 15:22:10 -0400

    Added a name and category label/text field to the activity view controller and made the category text field's input view a UIPickerView with a toolbar

--- iOS/Time/Time/ActivityViewController.swift
+++ iOS/Time/Time/ActivityViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class ActivityViewController: UIViewController {
+class ActivityViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    var categories = [ "Add a category...", "One", "Two", "Three" ]
+
+    // MARK: - Outlets
+    @IBOutlet weak var categoryPicker: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +21,36 @@ class ActivityViewController: UIViewController {
         // Do any additional setup after loading the view.
 
         self.title = "Activity"
+
+        // Set up the category picker and its toolbar
+        let categoryPickerView = UIPickerView()
+        
+        categoryPickerView.delegate = self
+
+        let toolbar = UIToolbar()
+
+        toolbar.barStyle = UIBarStyle.Default
+        toolbar.translucent = true
+        toolbar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done",
+                                         style: UIBarButtonItemStyle.Plain,
+                                         target: self,
+                                         action: "donePicker")
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace,
+                                         target: nil,
+                                         action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel",
+                                           style: UIBarButtonItemStyle.Plain,
+                                           target: self,
+                                           action: "cancelPicker")
+        
+        toolbar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolbar.userInteractionEnabled = true
+        
+        categoryPicker.inputView = categoryPickerView
+        categoryPicker.inputAccessoryView = toolbar
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,5 +68,40 @@ class ActivityViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+    // MARK: - UIPickerViewDataSource
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+
+    // MARK: - UIPickerViewDelegate
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (row != 0) {
+            categoryPicker.text = categories[row]
+        } else {
+            categoryPicker.text = ""
+
+            // TODO
+        }
+    }
+
+    // MARK: - Actions
+    func cancelPicker() {
+        categoryPicker.text = ""
+
+        donePicker()
+    }
+
+    func donePicker() {
+        categoryPicker.resignFirstResponder()
+    }
 
 }
