65000cd285843efec54f880a244903bb982f84f2
Davide Bruzzone <davide.bruzzone@gmail.com>
2015-11-04 19:59:21 -0500

    Created the 'Measure/Microsoft-Band' sub-project - It'll contain my Microsoft Band experiments

---
+++ iOS/Measure/Microsoft-Band/Microsoft-BandTests/Microsoft_BandTests.swift
@@ -0,0 +1,36 @@
+//
+//  Microsoft_BandTests.swift
+//  Microsoft-BandTests
+//
+//  Created by Davide Bruzzone on 11/4/15.
+//  Copyright © 2015 Bitwise Samurai. All rights reserved.
+//
+
+import XCTest
+@testable import Microsoft_Band
+
+class Microsoft_BandTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
