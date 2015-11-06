63e3b93a7f917c2aa361e100a733e803117767d1
Davide Bruzzone <davide.bruzzone@gmail.com>
2015-11-06 15:13:23 -0500

    Added code that connects to the first Microsoft Band that the app finds

--- iOS/Measure/Microsoft-Band/Microsoft-Band/ViewController.swift
+++ iOS/Measure/Microsoft-Band/Microsoft-Band/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MSBClientManagerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let sharedManager = MSBClientManager.sharedManager()
+
+        sharedManager.delegate = self
+
+        let attachedClients = sharedManager.attachedClients()
+
+        if attachedClients.count == 1 {
+            let client = attachedClients.first as! MSBClient
+
+            sharedManager.connectClient(client)
+        } else {
+            // TODO
+            for attachedClient in attachedClients {
+                print("Client name/description: \(attachedClient.name)/\(attachedClient.description)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - MSBClientManagerDelegate
+    
+    func clientManager(clientManager: MSBClientManager!, clientDidConnect client: MSBClient!) {
+        print("clientDidConnect")
+    }
+
+    func clientManager(clientManager: MSBClientManager!, clientDidDisconnect client: MSBClient!) {
+        print("clientDidDisconnect")
+    }
+
+    func clientManager(clientManager: MSBClientManager!, client: MSBClient!, didFailToConnectWithError error: NSError!) {
+        print("clientDidFailToConnectWithError")
+    }
 
 }
-
