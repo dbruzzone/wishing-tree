8fa092a2cc087303ecaca27b0b58ebb5b1dc8215
Davide Bruzzone <davide.bruzzone@gmail.com>
2015-11-05 20:02:42 -0500

    Configured the 'Measure/Microsoft-Band' sub-project to use the Microsoft Band SDK (but didn't commit the SDK)

---
+++ iOS/Measure/Microsoft-Band/Microsoft-Band/Microsoft-Band-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Microsoft-Band-Bridging-Header.h
+//  Microsoft-Band
+//
+//  Created by Davide Bruzzone on 11/5/15.
+//  Copyright © 2015 Bitwise Samurai. All rights reserved.
+//
+
+#ifndef Microsoft_Band_Bridging_Header_h
+
+#define Microsoft_Band_Bridging_Header_h
+
+#endif /* Microsoft_Band_Bridging_Header_h */
+
+#import <MicrosoftBandKit_iOS/MicrosoftBandKit_iOS.h>
