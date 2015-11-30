5644472a2370ca9e40fcdbdde56ae6f2d78e8fcd
Davide Bruzzone <davide.bruzzone@gmail.com>
2015-11-30 11:09:10 +0100

    Created a custom view for line-related experiments and connected it to the view controller that's shown when users tap the initial table view's 'Lines' cell

---
+++ iOS/Display/Display/LinesView.swift
@@ -0,0 +1,21 @@
+//
+//  LinesView.swift
+//  Display
+//
+//  Created by Davide Bruzzone on 11/30/15.
+//  Copyright © 2015 Bitwise Samurai. All rights reserved.
+//
+
+import UIKit
+
+class LinesView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
