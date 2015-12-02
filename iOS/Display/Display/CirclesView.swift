e000ba8cab0964930950966d9f6a9c2ae0b18f4d
Davide Bruzzone <davide.bruzzone@gmail.com>
2015-12-02 11:34:37 -0500

    Created a custom view for circle-related experiments and connected it to the view controller that's shown when users tap the initial table view's 'Circles' cell

---
+++ iOS/Display/Display/CirclesView.swift
@@ -0,0 +1,21 @@
+//
+//  CirclesView.swift
+//  Display
+//
+//  Created by Davide Bruzzone on 12/1/15.
+//  Copyright © 2015 Bitwise Samurai. All rights reserved.
+//
+
+import UIKit
+
+class CirclesView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
