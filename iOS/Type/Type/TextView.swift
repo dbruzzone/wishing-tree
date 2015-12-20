9c34d483069673fa5c64b365285ca1cbc24de21b
Davide Bruzzone <davide.bruzzone@gmail.com>
2015-12-20 15:28:34 -0500

    Created a custom view to display the letters and words that users must type

---
+++ iOS/Type/Type/TextView.swift
@@ -0,0 +1,21 @@
+//
+//  TextView.swift
+//  Type
+//
+//  Created by Davide Bruzzone on 12/20/15.
+//  Copyright © 2015 Bitwise Samurai. All rights reserved.
+//
+
+import UIKit
+
+class TextView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+    // Drawing code
+    }
+    */
+
+}
