251d7403ce3ac8fba3b074df3f480115b2d8d2da
Davide Bruzzone <davide.bruzzone@gmail.com>
2015-12-21 15:22:27 -0500

    Added code that draws a string in the center of the custom view that displays the letters and words that users must type

--- iOS/Type/Type/TextView.swift
+++ iOS/Type/Type/TextView.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class TextView: UIView {
 
-    /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
-    // Drawing code
+        // TODO: Turn the method's hard-coded values into properties
+        let text: NSString = "Hello"
+
+        // Set text's color
+        let textColor: UIColor = UIColor.darkGrayColor()
+        
+        // Set text's font
+        let textFont = UIFont(name: "Helvetica Neue", size: 20)
+
+        // Set text's obliqueness (0 indicates no obliqueness/skew)
+        let skew = 0.1
+        
+        let attributes: NSDictionary = [
+            NSForegroundColorAttributeName: textColor,
+            NSObliquenessAttributeName: skew,
+            NSFontAttributeName: textFont!
+        ]
+
+        let boundingBox: CGSize = text.sizeWithAttributes(attributes as? [String : AnyObject])
+        let offsets = xAndYOffsets(boundingBox, rect: rect)
+
+        text.drawInRect(CGRectMake(offsets.x, offsets.y, boundingBox.width, boundingBox.height), withAttributes: attributes as? [String : AnyObject])
     }
-    */
+
+    // MARK: - Private methods
+
+    private func xAndYOffsets(boundingBox: CGSize, rect: CGRect) -> (x: CGFloat, y: CGFloat) {
+        return (x: (rect.size.width - boundingBox.width) / 2.0,
+                y: (rect.size.height - boundingBox.height) / 2.0)
+    }
 
 }
