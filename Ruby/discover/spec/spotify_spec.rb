01b584087a21b8e00503a950d38b9090c8d7d4d8
Davide Bruzzone <davide.bruzzone@gmail.com>
2016-01-01 20:00:33 -0500

    Completed the tests and code for the '/login' route

--- Ruby/discover/spec/spotify_spec.rb
+++ Ruby/discover/spec/spotify_spec.rb
@@ -9,13 +9,19 @@ describe "My Spotify application" do
     expect(last_response.body).to match /Hello/
   end
 
-  it "should redirect users when they log in" do
+  it 'should redirect users when they log in' do
     get '/login'
 
     expect(last_response).to be_redirect
 
     follow_redirect!
 
-    expect(last_request.url).to eq 'https://accounts.spotify.com/authorize'
+    expect(last_request.url).to match /https:\/\/accounts\.spotify\.com\/authorize\?response_type=code&client_id=.{32}&scope=user-read-private\+user-read-email&redirect_uri=http%3A%2F%2Flocalhost%3A4567%2Fauth%2Fspotify%2Fcallback&state=[a-zA-Z0-9]{16}/
+  end
+
+  it "should set the 'spotify_auth_state' cookie redirect when users log in" do
+    get '/login'
+
+    expect(rack_mock_session.cookie_jar['spotify_auth_state']).to match /[a-zA-Z0-9]{16}/
   end
 end
--- Ruby/discover/spotify.rb
+++ Ruby/discover/spotify.rb
@@ -1,9 +1,36 @@
 require 'sinatra'
 
+require_relative 'lib/string_utils'
+require_relative 'lib/url_utils'
+
+# There are other/better ways to configure the server's settings (such as dotenv -
+# https://github.com/bkeepers/dotenv) but for now, since I'm  starting by porting the Spotify Web
+# API example at https://github.com/spotify/web-api-auth-examples/tree/master/authorization_code,
+# I'm keeping things simple
+client_id = ENV['DISCOVER_SPOTIFY_CLIENT_ID']
+redirect_uri = 'http://localhost:4567/auth/spotify/callback'
+
+state_key = 'spotify_auth_state'
+
 get '/' do
   'Hello'
 end
 
 get '/login' do
-  redirect 'https://accounts.spotify.com/authorize?'
+  state = StringUtils.generate_random_string(16)
+
+  response.set_cookie state_key, state
+
+  scope = 'user-read-private user-read-email'
+
+  query_parameters = {
+    response_type: 'code',
+    client_id: client_id,
+    scope: scope,
+    redirect_uri: redirect_uri,
+    state: state
+  }
+
+  # Request authorization
+  redirect "https://accounts.spotify.com/authorize?#{URLUtils::query_string(query_parameters)}"
 end
