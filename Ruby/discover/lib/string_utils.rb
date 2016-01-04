5653ef60a2744ae2001c5f30185ae93516e36ac1
Davide Bruzzone <davide.bruzzone@gmail.com>
2016-01-04 16:27:51 -0500

    Fixed an issue that was caused by incorrect (and randomly occurring) possible character array indices; the tests would fail when the code tried to access the last element in the possible characters array (because its index was 1 higher than it was supposed to be)

--- Ruby/discover/lib/string_utils.rb
+++ Ruby/discover/lib/string_utils.rb
@@ -5,7 +5,7 @@ class StringUtils
     possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
     length.times do
-      text << possible[ (rand.round(2) * possible.length).floor ]
+      text << possible[ (rand.round(2) * possible.length).floor - 1 ]
     end
 
     text
