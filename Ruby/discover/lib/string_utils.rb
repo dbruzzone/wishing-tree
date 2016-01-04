489953bac453d3b93842114b794353d95f0837db
Davide Bruzzone <davide.bruzzone@gmail.com>
2016-01-04 16:36:28 -0500

    Cleaned up a few lines of code in a couple of files

--- Ruby/discover/lib/string_utils.rb
+++ Ruby/discover/lib/string_utils.rb
@@ -1,8 +1,7 @@
 class StringUtils
   def self.generate_random_string(length)
-    text = ''
-
     possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    text = ''
 
     length.times do
       text << possible[ (rand.round(2) * possible.length).floor - 1 ]
--- Ruby/discover/spec/spotify_spec.rb
+++ Ruby/discover/spec/spotify_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
 describe "My Spotify application" do
-  it "should allow users to access the home page" do
+  it 'should allow users to access the home page' do
     get '/'
 
     expect(last_response).to be_ok
@@ -19,7 +19,7 @@ describe "My Spotify application" do
     expect(last_request.url).to match /https:\/\/accounts\.spotify\.com\/authorize\?response_type=code&client_id=.{32}&scope=user-read-private\+user-read-email&redirect_uri=http%3A%2F%2Flocalhost%3A4567%2Fauth%2Fspotify%2Fcallback&state=[a-zA-Z0-9]{16}/
   end
 
-  it "should set the 'spotify_auth_state' cookie redirect when users log in" do
+  it "should set the 'spotify_auth_state' cookie when users log in" do
     get '/login'
 
     expect(rack_mock_session.cookie_jar['spotify_auth_state']).to match /[a-zA-Z0-9]{16}/
