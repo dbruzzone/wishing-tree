c12ae3df8b6a07481953af36c210d7a210509514
Davide Bruzzone <davide.bruzzone@gmail.com>
2016-01-07 19:33:35 -0500

    Updated the code to send its request via HTTPS. Note: In order to do this, it's necessary to specify the location of the certificate that mitmproxy generates

--- JavaScript/Node.js/discover/proxy-request.js
+++ JavaScript/Node.js/discover/proxy-request.js
@@ -1,10 +1,19 @@
-var request = require('request');
+var fs = require('fs'),
+    path = require('path'),
+    request = require('request');
+
+var caFile = path.resolve(__dirname, 'ssl/mitmproxy-ca-cert.pem');
 
 var req = request.defaults({
   proxy: 'http://localhost:8080'
 });
 
-req.get({ url: 'http://www.google.com/' }, function (error, response, user) {
+var options = {
+    url: 'https://www.google.com/',
+    ca: fs.readFileSync(caFile)
+};
+
+req.get(options, function (error, response, user) {
   var errorPrefix = '- Error: ';
 
   error !== null ? console.log(errorPrefix + error) : console.log(errorPrefix + 'None');
