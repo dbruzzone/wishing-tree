9e44448bddb551140979372238349256b10c0acd
Davide Bruzzone <davide.bruzzone@gmail.com>
2016-01-11 21:00:52 -0500

    Started building the server that sends HTML e-mails using a mail service

---
+++ JavaScript/Node.js/mail-monkey/server.js
@@ -0,0 +1,30 @@
+var fs = require('fs'),
+    nconf = require('nconf');
+
+nconf.env().file({ file: 'config.json' });
+
+var express = require('express');
+
+var app = express();
+
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+// App configuration
+app.use(morgan('dev')); // Log every request to the console
+
+app.use(bodyParser.urlencoded({ extended: false })); // Handle 'application/x-www-form-urlencoded' forms
+
+// Routes
+app.get('/', function (req, res) {
+  // TODO
+  res.send('Hello');
+});
+
+var port = nconf.get('port') || 3000;
+
+var server = app.listen(port, function () {
+  var serverAddress = server.address();
+
+  console.log('The mail-monkey app is listening at http://%s:%s', serverAddress.host, serverAddress.port);
+});
